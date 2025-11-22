a4e87a301924a8844dd26a283d1ca8c052400f74
m3nd0zit4 <julianestebanmb2007@gmail.com>
2025-11-22 11:55:31 -0500

    About section but with a new component a reusable text animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return ( 
     <div className="relative w-screen min-h-screen overflow-x-hidden">
       <Hero />
 
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About />
     </div>
    );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,54 @@
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#clip",
+                start: "center center",
+                end: "+=800 center",
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+            }
+        })
+
+        clipAnimation.to('.mask-clip-path', {
+            width: '100vw',
+            height: '100vh',
+            borderRadius: 0,
+        })
+    });
+
+    return ( 
+        <div id="about" className="w-screen min-h-screen">
+            <div className="relative flex flex-col items-center gap-5 mb-8 mt-36">  
+                <h2 className="font-general text-sm uppercase md:text-[10px]"> Welcome to VALORANT</h2>
+
+                <AnimatedTitle title="Enter to this new seson <br /> of epic <br /> caracters and maps" containerClass="mt-5 !text-red-500 text-center"/>
+
+                <div className="about-subtext">
+                    <p>An epic adventure awaits here </p>
+                    <p>This journey will be different for all players</p>
+                </div>
+            </div>
+
+            <div className="w-screen h-dvh" id="clip">
+                <div className="mask-clip-path about-image">
+                    <img 
+                        src="img/about.webp"
+                        alt="Background"
+                        className="absolute top-0 left-0 object-cover size-full"
+                    />
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default About;
