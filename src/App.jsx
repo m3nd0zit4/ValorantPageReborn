a24fb00511b7c5fbf8fe4b573909439e9ed87d35
m3nd0zit4 <julianestebanmb2007@gmail.com>
2025-11-24 19:19:46 -0500

    Navbar section with sound and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return ( 
     <div className="relative w-screen min-h-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
 
       <About />
